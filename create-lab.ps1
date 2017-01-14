376754d54585c05e25b634a70cc3032e56e2e5ca
Jörg Zimmermann <Jrg Zimmermann>
2017-01-14 21:32:40 +0100

    pester tests, helperfunctions, class Node

--- create-lab.ps1
+++ create-lab.ps1
@@ -1,7 +1,13 @@
+#Requires -Version 5
+#Requires -Modules xComputerManagement
+#Requires -RunasAdministrator
 param (
     $WORKDIR = ".\",
     $ConfigurationPath = ".\configuration.xlsx"
  )
+
+#region Includes and imports
+ . .\convert-windowsimage.ps1
 
 function get-ExcelDataHashTable {
     #written by Jörg Zimmermann
@@ -45,6 +51,91 @@ function get-ExcelDataHashTable {
     $DataHashTable
  }
 
+Function Set-VMNetworkConfiguration {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true,
+                   Position=1,
+                   ParameterSetName='DHCP',
+                   ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ParameterSetName='Static',
+                   ValueFromPipeline=$true)]
+        [Microsoft.HyperV.PowerShell.VMNetworkAdapter]$NetworkAdapter,
+
+        [Parameter(Mandatory=$true,
+                   Position=1,
+                   ParameterSetName='Static')]
+        [String[]]$IPAddress=@(),
+
+        [Parameter(Mandatory=$false,
+                   Position=2,
+                   ParameterSetName='Static')]
+        [String[]]$Subnet=@(),
+
+        [Parameter(Mandatory=$false,
+                   Position=3,
+                   ParameterSetName='Static')]
+        [String[]]$DefaultGateway = @(),
+
+        [Parameter(Mandatory=$false,
+                   Position=4,
+                   ParameterSetName='Static')]
+        [String[]]$DNSServer = @(),
+
+        [Parameter(Mandatory=$false,
+                   Position=0,
+                   ParameterSetName='DHCP')]
+        [Switch]$Dhcp
+    )
+
+    $VM = Get-WmiObject -Namespace 'root\virtualization\v2' -Class 'Msvm_ComputerSystem' | Where-Object { $_.ElementName -eq $NetworkAdapter.VMName } 
+    $VMSettings = $vm.GetRelated('Msvm_VirtualSystemSettingData') | Where-Object { $_.VirtualSystemType -eq 'Microsoft:Hyper-V:System:Realized' }    
+    $VMNetAdapters = $VMSettings.GetRelated('Msvm_SyntheticEthernetPortSettingData') 
+
+    $NetworkSettings = @()
+    foreach ($NetAdapter in $VMNetAdapters) {
+        if ($NetAdapter.Address -eq $NetworkAdapter.MacAddress) {
+            $NetworkSettings = $NetworkSettings + $NetAdapter.GetRelated("Msvm_GuestNetworkAdapterConfiguration")
+        }
+    }
+
+    $NetworkSettings[0].IPAddresses = $IPAddress
+    $NetworkSettings[0].Subnets = $Subnet
+    $NetworkSettings[0].DefaultGateways = $DefaultGateway
+    $NetworkSettings[0].DNSServers = $DNSServer
+    $NetworkSettings[0].ProtocolIFType = 4096
+
+    if ($dhcp) {
+        $NetworkSettings[0].DHCPEnabled = $true
+    } else {
+        $NetworkSettings[0].DHCPEnabled = $false
+    }
+
+    $Service = Get-WmiObject -Class "Msvm_VirtualSystemManagementService" -Namespace "root\virtualization\v2"
+    $setIP = $Service.SetGuestNetworkAdapterConfiguration($VM, $NetworkSettings[0].GetText(1))
+
+    if ($setip.ReturnValue -eq 4096) {
+        $job=[WMI]$setip.job 
+
+        while ($job.JobState -eq 3 -or $job.JobState -eq 4) {
+            start-sleep 1
+            $job=[WMI]$setip.job
+        }
+
+        if ($job.JobState -eq 7) {
+            write-host "Success"
+        }
+        else {
+            $job.GetError()
+        }
+    } elseif($setip.ReturnValue -eq 0) {
+        Write-Verbose "Success"
+    }
+ }
+
+
 class Network {
 
     [STRING]$IPAddress
@@ -68,6 +159,8 @@ class Network {
         return $Hash
     }
  }
+
+
 
 
 class Node {
@@ -143,6 +236,12 @@ class Node {
 
         if ($Hash.contains("ConfigurationStandards")) {
             $this | Add-Member -MemberType NoteProperty -Name "ConfigurationStandards" -value $Hash.ConfigurationStandards -force
+            $Paths = ($this.ConfigurationStandards.keys).where({$_ -match "Path"})
+            $Paths | ForEach-Object{
+                if ( !(Test-Path -Path $this.ConfigurationStandards.$_.value) ) {
+                    mkdir -Path $this.ConfigurationStandards.$_.value -Force
+                } 
+            }
         }
     }
 
@@ -227,8 +326,147 @@ class Node {
 
     exportXML([string]$path){
         $this | Export-Clixml -Path $path
+    
+    }
+     [Microsoft.Vhd.PowerShell.VirtualHardDisk]getMasterImage(){
+    
+        return (get-vhd -Path $this.TemplatePath)
+       
      }
 
+    [Microsoft.Vhd.PowerShell.VirtualHardDisk]createMasterImage(){
+        
+        #create MasterImage from ISO and integration of Packages
+        $ISO = $null
+        $VHDPATH = $null
+        if ($this.OS -eq "Windows Server 2012 R2") {
+            $ISO=$this.ConfigurationStandards.ISOPath.Value+"\"+$this.ConfigurationStandards.ISO_Windows2012R2.value
+            $VHDPATH = $this.ConfigurationStandards.HyperVVHDPath.Value+"\"+$this.ConfigurationStandards.template_Windows2012R2.Value
+        }
+        elseif ($this.OS -eq "Windows Server 2012") {
+            $ISO=$this.ConfigurationStandards.ISOPath.Value+"\"+$this.ConfigurationStandards.ISO_Windows2012.value
+            $VHDPATH = $this.ConfigurationStandards.HyperVVHDPath.Value+"\"+$this.ConfigurationStandards.template_Windows2012.Value
+        }
+        else {
+            Write-Error ($this.OS+" is not a valid OS")
+        }
+        
+        $ISOItem = Test-Path -Path $ISO
+        $VHDPATHItem = Test-Path -Path $VHDPATH
+        if (!$ISOItem) {
+            Write-Error "$ISO not found"
+        }
+
+        if ($VHDPATH) {
+            Write-Information "$VHDPATH exists"
+        }
+
+        if (($ISOItem) -and (!$VHDPATHItem) ) {
+            $WIM = ((Mount-DiskImage -ImagePath $ISO -StorageType ISO -PassThru | Get-Volume).DriveLetter+":\sources\install.wim")
+            Convert-WindowsImage -SourcePath $WIM -VHDPath $VHDPATH -Edition ($this.OS+" SERVERDATACENTER") -VHDFormat VHDX -Verbose -VHDType Dynamic -SizeBytes 50GB -VHDPartitionStyle MBR
+            dismount-DiskImage $ISO
+            <# ToDo integration of Packages and Updates 
+            Put the Dism-Magic here:
+            Mount VHDX
+            integrate Packages and Drivers
+            dismount vhdx
+            #>
+            $this | Add-Member -MemberType NoteProperty -Name TemplatePath -Value $VHDPATH
+        }
+
+
+        return $this.getMasterImage()
+    }
+
+    [Microsoft.Vhd.PowerShell.VirtualHardDisk] createVHD(){
+        #copy the MasterImage with new name to new filelocation
+        $VHDPath = Join-Path -path $this.ConfigurationStandards.HyperVVHDPath.Value -ChildPath ($this.Name,"vhdx" -join(".")) 
+        $TempplatePath = ($this.getMasterImage()).Path
+        if (!$TempplatePath) {
+            Write-Error "$TempplatePath not found"
+        }
+        if (!$this.getVHD()) {
+            Copy-Item -Path $TempplatePath -Destination $VHDPath -Verbose -Force
+            $this | Add-Member -MemberType NoteProperty -Name VHDPath -Value $VHDPath
+        }
+        return $this.getVHD()
+    }
+
+    [Microsoft.Vhd.PowerShell.VirtualHardDisk] getVHD(){
+        return (get-vhd -path $this.VHDPath)        
+    }
+
+    [Microsoft.HyperV.PowerShell.VirtualMachine]createVM(){
+        $VHDPath = $this.getVHD().Path
+        $VM = $null
+        if ($this.isVirtual) {
+            try {
+                get-vm -id $this.VMId -ErrorAction Stop
+            }
+            catch [Microsoft.HyperV.PowerShell.VirtualizationManagementException] {
+                $VM = New-VM -Name $this.Name -MemoryStartupBytes ($this.memory+"MB") -Generation 1 -Path $this.ConfigurationStandards.hypervvmpath.value -VHDPath $VHDPath
+                $this | Add-Member -MemberType NoteProperty -name VMId -Value $VM.VMId -Force
+            }
+        }
+
+        return $this.getVM()
+    }
+
+    [Microsoft.HyperV.PowerShell.VirtualMachine]getVM(){
+        return (get-vm -Name $this.Name)
+    }
+     
+    [xml] unattendXML(){
+        [xml] $unattend= [xml]"<?xml version='1.0' encoding='utf-8'?>
+        <unattend xmlns='urn:schemas-microsoft-com:unattend'>
+          <settings pass='oobeSystem'>
+            <component name='Microsoft-Windows-International-Core' processorArchitecture='amd64' publicKeyToken='31bf3856ad364e35' language='neutral' versionScope='nonSxS' xmlns:wcm='http://schemas.microsoft.com/WMIConfig/2002/State' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'>
+              <InputLocale></InputLocale>
+              <SystemLocale></SystemLocale>
+              <UILanguage></UILanguage>
+              <UserLocale></UserLocale>
+            </component>
+            <component name='Microsoft-Windows-Shell-Setup' processorArchitecture='amd64' publicKeyToken='31bf3856ad364e35' language='neutral' versionScope='nonSxS' xmlns:wcm='http://schemas.microsoft.com/WMIConfig/2002/State' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'>
+              <OOBE>
+                <HideEULAPage>true</HideEULAPage>
+              </OOBE>
+              <AutoLogon>
+                <Password>
+                  <Value></Value>
+                  <PlainText></PlainText>
+                </Password>
+                <Enabled>true</Enabled>
+                <LogonCount>1</LogonCount>
+                <Username>Administrator</Username>
+              </AutoLogon>
+            <FirstLogonCommands>
+                <SynchronousCommand wcm:action='add'>
+                  <CommandLine>%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy RemoteSigned -File c:\scripts\start-config.ps1</CommandLine>
+                  <Description>Configuration in Progress</Description>
+                  <Order>2</Order>
+                  <RequiresUserInput>false</RequiresUserInput>
+                </SynchronousCommand>
+                <SynchronousCommand wcm:action='add'>
+                  <CommandLine>cmd /c reg add 'HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell' /v ExecutionPolicy /t REG_SZ /d RemoteSigned /f</CommandLine>
+                  <Description>setting Executionpolicy</Description>
+                  <Order>1</Order>
+                </SynchronousCommand>
+              </FirstLogonCommands>
+              <UserAccounts>
+                <AdministratorPassword>
+                  <Value></Value>
+                  <PlainText>true</PlainText>
+                </AdministratorPassword>
+              </UserAccounts>
+              <RegisteredOwner>-</RegisteredOwner>
+              <RegisteredOrganization>-</RegisteredOrganization>
+            </component>
+          </settings>
+          <cpi:offlineImage cpi:source='wim:d:/clusterinstall/install.wim#Windows Server 2012 SERVERDATACENTERCORE' xmlns:cpi='urn:schemas-microsoft-com:cpi' />
+        </unattend>"  
+        
+        return $unattend
+    }
     
 }
 
@@ -257,16 +495,26 @@ class MyConfiguration {
             break
         }
     }
+
     init([Hashtable]$Hash){
         $Hash.keys | ForEach-Object {
             if ( ($_ -notmatch "Networks") -and ($_ -notmatch "ConfigurationStandards")){
-                
                 $Layer = $_
-
                 ($Hash.$Layer).keys | ForEach-Object {
-                    
                     $this.add($_,$Hash)
                 }
+            }
+        }
+        if ($Hash.Contains("ConfigurationStandards")) {
+            #find VMHosts for Infra an set it in Node-Object ConfigurationStandards
+            $VMHosts = ($this.nodes.where({($_.Layer -match "INFRA") -and ($_.roles -match "Hyper-V")})).Name -join(",")
+            $this.nodes.where({($_.Layer -match "INFRA") -or ($_.Layer -match "HSA") -or ($_.Layer -match "CM")}) | ForEach-Object {
+                $_ | Add-Member -MemberType NoteProperty -Name VMHosts -Value $VMHosts
+            }
+            #find VMHosts for SSA an set it in Node-Object ConfigurationStandards
+            $VMHosts = $this.nodes.where({($_.Layer -match "SSA") -and ($_.roles -match "Hyper-V")}) -join(",")
+            $this.nodes.where({($_.Layer -match "SSA")}) | ForEach-Object {
+                $_ | Add-Member -MemberType NoteProperty -Name VMHosts -Value $VMHosts
             }
         }
     }
@@ -328,7 +576,20 @@ class MyConfiguration {
 
  }
 
+#region Configurations 
+Configuration set-Computername {
+    param(
+        [String]$Computername,
+        [String]$NodeName
+    )
+    Import-DscResource -Module xComputerManagement
+   
+    Node $NodeName {
+        xComputer NewName {
+            Name = $Computername
+        }
+        
+    }
+}
 
 
-$MyConfiguration = [Myconfiguration]::new( '.\Configuration.xlsx')
-$MyConfiguration.getConfigurationData()
---
+++ create-lab.tests.ps1
@@ -0,0 +1,91 @@
+﻿
+. ./create-lab.ps1
+
+Describe "InputString ConfigurationPath"{
+    it "should contain a String" {
+        $ConfigurationPath = ".\Configuration.xlsx"
+        $ConfigurationPath | should beOfType [String]
+        
+    }
+    it "should get an item" {
+        get-item $ConfigurationPath | should beOfType [System.IO.FileInfo]
+    }
+
+    it "should be the Item of that string"{
+        (get-item $ConfigurationPath).FullName | Should beLike "*$ConfigurationPath*"
+    }
+}
+
+Describe "get-exceldatahashtable" {
+    Context "get-exceldatahashtable returns"{
+        $Configuration = get-exceldatahashtable -path $configurationPath
+        it 'get-exceldatahashtable -path [$configurationPath] should return a Hashtable' {            
+            $Configuration | Should beOfType [Hashtable]
+        }
+        $Configuration.keys | ForEach-Object {
+            $Key = $_
+            it "$Key should return a Hashtable" {
+                $Configuration.$Key | Should beOfType [Hashtable]
+            }
+            $Configuration.$Key.Keys | ForEach-Object {
+                it "$_ should return a Hashtable" {
+                    $Configuration.$Key.$_ | Should beOfType [Hashtable]
+                }
+            }
+        }
+    }
+}
+
+Describe "Class node" {
+    Context "Constructors" {
+        $Configuration = get-exceldatahashtable -path $configurationPath
+        it '[Node]::new("Name") should return a Node Object' {
+            [Node]::new("Name") | should beOfType [Node]
+        }
+        it '[Node]::new("Y02PINHST001",$Configuration) should return a Node Object' {
+            
+            [Node]::new("Y02VINPDC001",$Configuration) | should beOfType [Node]
+        }
+    }
+    Context "Methods" {
+        $Configuration = get-exceldatahashtable -path $configurationPath
+        $Node = [Node]::new("Y02VINPDC001",$Configuration)
+        it '$Node should be a Node Object'{
+            $Node | should beOfType [Node]
+        }
+        it '$Node.getMasterImage() should return a VHD'{
+            $Node.getmasterimage() | Should beOfType [Microsoft.Vhd.PowerShell.VirtualHardDisk]
+        }
+        it '$Node.getvhd() should return a VHD' {
+            $Node.getvhd() | Should beOfType [Microsoft.Vhd.PowerShell.VirtualHardDisk]
+        }
+    }
+}
+
+Describe "Class MyConfiguration"{
+    Context "Constructors"{
+        $MyConfiguration = [MyConfiguration]::new($ConfigurationPath)
+        it 'new([String]) should return Type MyConfiguration' {
+            $MyConfiguration | should beOfType [MyConfiguration]
+        }
+        it "Property Nodes should be of Type [Node]" {
+            $MyConfiguration.Nodes | should beOfType [Node]
+        }
+
+        it "every Node has to have a property ConfigurationStandards" {
+            $MyConfiguration.nodes | ForEach-Object {
+                ($_ | Get-Member -Name ConfigurationStandards).Name | Should be "ConfigurationStandards"
+            }
+        }
+        it "every Node has to have a property Networks" {
+            $MyConfiguration.nodes | ForEach-Object {
+                ($_ | Get-Member -Name Networks).Name | Should be "Networks"
+            }
+        }
+        it "every Node's Networks Property has to have a Network Object inside" {
+            $MyConfiguration.nodes | ForEach-Object {
+            $_.Networks | Should beOfType [Network]
+            }
+        }
+    }
+}
